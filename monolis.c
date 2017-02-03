d72d8bb222e9a54e9c1fd4f4cb8fab9ba4a97472
ichisemasashi <ichise.masashi@gmail.com>
2017-02-03 20:48:42 +0900

    a little change.

--- monolis.c
+++ monolis.c
@@ -5,12 +5,18 @@
 */
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
 /* 関数一覧 */
 void initcell (void);
 int makesym(char *name);
 void assocsym(int sym, int val);
 int cons(int car, int cdr);
+void gettoken(void);
+int numbertoken(char buf[]);
+int symboltoken(char buf[]);
+int issymch(char c);
 
 /* セル構造 */
 typedef enum tag {EMP,NUM,SYM,LIS,SUBR,FSUBR,FUNC} tag;
@@ -93,3 +99,139 @@ int cons(int car, int cdr){
     SET_CDR(addr,cdr);
     return(addr);
 }
+
+
+typedef enum toktype {LPAREN,RPAREN,QUOTE,DOT,NUMBER,SYMBOL,OTHER} toktype;
+typedef enum backtrack {GO,BACK} backtrack;
+#define BUFFSIZE 256
+
+struct token {
+	char ch;
+	backtrack flag;
+	toktype type;
+	char buf[BUFFSIZE];
+} stok;
+
+#define EOL     '\n'
+#define TAB     '\t'
+#define SPACE   ' '
+#define NUL     '\0'
+
+void gettoken (void) {
+	char c;
+	int pos;
+
+	if(stok.flag == BACK) {
+		stok.flag = GO;
+		return;
+	}
+	if (stok.ch == ')') {
+		stok.type = RPAREN;
+		stok.ch = NIL;
+		return;
+	}
+	if (stok.ch == '(') {
+		stok.type = LPAREN;
+		stok.ch = NIL;
+		return;
+	}
+	c = getchar();
+	while ((c == SPACE) || (c == EOL) || (c == TAB)) {
+		c = getchar();
+	}
+	switch(c) {
+		case '(': stok.type = LPAREN; break;
+		case ')': stok.type = RPAREN; break;
+		case '\'': stok.type = QUOTE; break;
+		case '.': stok.type = DOT; break;
+		default: 
+		    pos = 0;
+		    stok.buf[pos++] = c;
+		    while ((c=getchar()!=EOL) &&
+		    	   (pos < BUFFSIZE) &&
+		    	   (c != SPACE) &&
+		    	   (c != '(') &&
+		    	   (c != ')')) {
+		    	stok.buf[pos++] = c;
+		    }
+		    if (numbertoken(stok.buf)) {
+		    	stok.type = NUMBER;
+		    	break;
+		    }
+		    if (symboltoken(stok.buf)) {
+		    	stok.type = SYMBOL;
+		    	break;
+		    }
+		    stok.type = OTHER;
+	}
+}
+int numbertoken(char buf[]){
+    int i;
+    char c;
+    
+    if((buf[0] == '+') || (buf[0] == '-')){
+        if(buf[1] == NUL)
+            return(0); // case {+,-} => symbol
+        i = 1;
+        while((c=buf[i]) != NUL)
+            if(isdigit(c))
+                i++;  // case {+123..., -123...}
+            else
+                return(0); 
+    }
+    else {       
+        i = 0;    // {1234...}
+        while((c=buf[i]) != NUL)
+            if(isdigit(c))
+                i++;
+            else 
+                return(0);
+    }
+    return(1);
+}
+
+int symboltoken(char buf[]){
+    int i;
+    char c;
+    
+    if(isdigit(buf[0]))
+        return(0);
+    
+    i = 0;
+    while((c=buf[i]) != NUL)
+        if((isalpha(c)) || (isdigit(c)) || (issymch(c)))
+            i++;
+        else 
+            return(0);
+    
+    return(1);
+}
+
+int issymch(char c){
+    switch(c){
+        case '!':
+        case '?':
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+        case '<':
+        case '>': return(1);
+        default:  return(0);
+    }
+}  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
